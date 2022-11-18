da1e5c54c8488ce09d948046fb105f9c7889ee1f
Ploy11112545 <64309010021@tnk.ac.th>
2022-11-18 10:22:54 +0700

    Create reviewJS.js

---
+++ week5/reviewJS.js
@@ -0,0 +1,10 @@
+x = 5;
+var y = 10;
+let myName = "PLOY";
+const year = 2022;
+//year = 2023;
+console.log("y เป็นชนิด ",typeof(y));
+console.log("myName เป็นชนิด ",typeof(myName));
+
+y = "JAVA";
+console.log(y,typeof(y));
