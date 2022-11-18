f72da5be5d50edde734eddf4dcffb65a985f8409
Ploy11112545 <64309010021@tnk.ac.th>
2022-11-18 08:53:36 +0700

    Create TestJS.js

---
+++ week4/TestJS.js
@@ -0,0 +1,14 @@
+var myName ='Ploy';
+console.log(myName);
+
+let lastName = 'Pongsa';
+const age = 20;
+//age = 26;
+
+var year = 2022;
+let grade = 3.96;
+
+grade = Math.floor(grade)
+console.log(grade)
+
+console.log(`My name is ${myName}`);
